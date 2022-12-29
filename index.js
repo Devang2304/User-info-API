0cee19c7e0be96e47bd934f091048525a39e56ce
Devang2304 <devangvartak2304@gmail.com>
2022-12-29 23:17:58 +0530

    created middleware and server

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express =require('express');
 const bodyParser = require('body-parser');
+const usersRoutes = require('./routes/users');
 
 const app = express();
 const PORT= 5000;
 
 //initialize the middleware
 app.use(bodyParser.json());
+app.use('/users',usersRoutes);
+
+// creating routes 
+app.get('/',(req,res)=>{
+    res.send('Hello from home page!');
+}); 
 
 // we dont want to restart the server when making changes so to avoid restarting the server time to
 //time we install nodemon package
