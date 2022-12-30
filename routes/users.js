437fbc2d2cd6adb24ec9dcc636d19a7d90dba964
Devang2304 <devangvartak2304@gmail.com>
2022-12-31 00:32:48 +0530

    created 2 routes 1.to add the new user to DB 2.Search user using their id

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,11 @@
 const express =require('express');
-
 const router= express.Router();
 
-const users =[
-    {
-        firstName:"John",
-        lastName:"Doe",
-        age:25
-    },
-    {
-        firstName:"Jane",
-        lastName:"Doe",
-        age:24
-    }
-]
+// installed uuid package which will create unique ID for
+//each user whenever we fetch request for particular user
+const {v4 : uuidv4} = require('uuid')
+
+const users =[];
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
@@ -23,7 +15,28 @@ router.get('/', (req, res) => {
 
 //posting the data to the server using post
 router.post('/', (req, res) => {
+    console.log('Post route reached');
+    const user =req.body;
+
+// '...user' that is '...' operator takes all of the 
+//properties of the variable used.
+    users.push({...user, id: uuidv4()});
+    res.send('Post route reached');
+});
+
+//checking if the user with the given id exists
+// router.get('/:id', (req, res) => {
+    //     console.log(users)
+    //     res.send(users.find(user => user.id === req.params.id));
+    // });
     
+//get particular user with given id
+// .users/2 => req.params { id:2 }
+router.get('/:id',(req,res)=>{
+    const {id} = req.params;
+    const founduser=users.find((user)=>user.id ===id);
+
+    res.send(founduser);
 });
 
 module.exports = router;
